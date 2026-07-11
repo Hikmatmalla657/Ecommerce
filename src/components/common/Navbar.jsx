f6e89f00bd36096cd6d1e0fa452a17da5f4024bc
Hikmatmalla657 <hikmatmalla657@gmail.com>
2026-07-11 11:32:11 +0545

    update

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -167,7 +167,7 @@ function Navbar() {
                 onClick={() => setIsMenuOpen(false)}
                 className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-full transition duration-200 text-center"
               >
-                Login
+                login
               </Link>
               <button
                 type="button"
